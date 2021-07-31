85f9bf211ecf293764bc5e80532a12ff9bcad938
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2021-07-31 20:36:08 +0530

    Add files via upload
    benchmark added

---
+++ src/main/java/com/cts/AuditSeverity/pojo/AuditType.java
@@ -0,0 +1,24 @@
+package com.cts.AuditSeverity.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+/**
+ * 
+ * AuditType POJO class
+ *
+ */
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class AuditType {
+	/**
+	 * Stores Type of Audit
+	 */
+	String auditType;
+
+}
