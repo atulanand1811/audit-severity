91f4a9fecf1c91ee5af2a37b217765debd5b2ae1
atulanand1811 <61856564+atulanand1811@users.noreply.github.com>
2021-08-01 16:42:41 +0530

    Update SeverityController.java
    cross-origin added

--- src/main/java/com/cts/AuditSeverity/SeverityController.java
+++ src/main/java/com/cts/AuditSeverity/SeverityController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -31,7 +32,7 @@ import lombok.extern.slf4j.Slf4j;
  *
  */
 
-
+@CrossOrigin(origins = "*")
 @RestController
 @Slf4j
 public class SeverityController {
